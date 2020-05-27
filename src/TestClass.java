30483d722da6b0df0fc4a4bbae89b940b5f43e68
serdyuuuk <63047668+serdyuuuk@users.noreply.github.com>
2020-05-27 19:08:35 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -10,12 +10,15 @@ public class TestClass {
     students[0].setName("Костя");
     students[1].setName("Алеша");
     students[2].setName("Яна");
-        students[3].setName("Їнв");
-        students[4].setName("Інб");
+        students[3].setName("Янв");
+        students[4].setName("Янб");
     sortNames(true,students);
     for (int i=0;i<5;i++){
         System.out.println(students[i].getName());
         }
+    Student[] find = findStudentsName(students,"Янв");
+    for (int i = 0; i<find.length;i++)
+        System.out.println(find[i].toString());
     }
 
     private static void swap(Student[] students, int i, int y) {
@@ -119,6 +122,50 @@ public class TestClass {
         value[88]=57;
         value[89]=58;
         value[90]=59;
+    }
+    private static Student[] findStudentsName(Student[] students, String name){
+        Student[] studentList = {};
+        for(int j = 0; j<students.length;j++) {
+            boolean find = false;
+            if (name.length()<=students[j].getName().length()) {
+                for (int i = 0; i < name.length(); i++) {
+                    if (students[j].getName().charAt(i) == name.charAt(i)) find = true;
+                    else {
+                        find = false;
+                        break;
+                    }
+                }
+            }
+            if (find==true){
+                Student[] studentListNow = new Student[studentList.length+1];
+                System.arraycopy(studentList,0,studentListNow,0,studentList.length);
+                studentListNow[studentList.length]=students[j];
+                studentList=studentListNow;
+            }
+        }
+        return studentList;
+    }
+    private static Student[] findStudentsLastName(Student[] students, String name){
+        Student[] studentList = {};
+        for(int j = 0; j<students.length;j++) {
+            boolean find = false;
+            if (name.length()<=students[j].getLastName().length()) {
+                for (int i = 0; i < name.length(); i++) {
+                    if (students[j].getLastName().charAt(i) == name.charAt(i)) find = true;
+                    else {
+                        find = false;
+                        break;
+                    }
+                }
+            }
+            if (find==true){
+                Student[] studentListNow = new Student[studentList.length+1];
+                System.arraycopy(studentList,0,studentListNow,0,studentList.length);
+                studentListNow[studentList.length]=students[j];
+                studentList=studentListNow;
+            }
+        }
+        return studentList;
     }
 }
 
