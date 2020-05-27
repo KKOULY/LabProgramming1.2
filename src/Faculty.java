1bb43676662ff8493b4bc32d70bc8501bd36b7f4
KKOULY <dmt.solovyov@gmail.com>
2020-05-27 17:43:02 +0300

    update Faculty

--- src/Faculty.java
+++ src/Faculty.java
@@ -8,6 +8,27 @@ public class Faculty {
         this.nameOfFaculty = nameOfFaculty;
     }
 
+    public void addDepartment(String nameOfDepartment){
+        Department[] departmentsTemporary = new Department[departments.length+1];
+        System.arraycopy(departments,0,departmentsTemporary,0,departments.length);
+        departmentsTemporary[departments.length] = new Department(nameOfDepartment);
+        departments = departmentsTemporary;
+    }
+
+    public void deleteDepartment(int numOfDepartment){
+        if (numOfDepartment<=departments.length && numOfDepartment>=0) {
+            Department[] departmentsTemporary = new Department[departments.length - 1];
+            int num = 0;
+            for (int i=0; i<departments.length;i++){
+                if (i!=numOfDepartment) {
+                    departmentsTemporary[num] = departments[i];
+                    num++;
+                }
+            }
+            departments = departmentsTemporary;
+        }
+    }
+
     public String toString(){
         return nameOfFaculty;
     }
