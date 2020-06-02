b00f4567ae4da46708115150d5bf669c50cc181a
KKOULY <dmt.solovyov@gmail.com>
2020-06-02 18:44:30 +0300

    java doc for Person/Student/Teacher

--- src/Student.java
+++ src/Student.java
@@ -1,30 +1,70 @@
 public class Student extends Person{
     private int course;
     private int group;
+
+    /**
+     * Конструктор
+     * @param name Імя
+     * @param lastName Прізвище
+     * @param personFaculty Факультет
+     * @param personDepartment Кафедра
+     */
     public Student(String name,String lastName,Faculty personFaculty,Department personDepartment){
     super(name,lastName,personFaculty,personDepartment);
     }
+
+    /**
+     * Конструктор
+     */
     public Student(){
 
     }
+
+    /**
+     * Конструктор
+     * @param name Імя
+     * @param lastName Прізвище
+     * @param personFaculty Факультет
+     * @param personDepartment Кафедра
+     * @param course курс
+     * @param group група
+     */
     public Student(String name,String lastName,Faculty personFaculty,Department personDepartment,int course, int group){
         super(name,lastName,personFaculty,personDepartment);
         setCourse(course);
         setGroup(group);
     }
+
+    /**
+     * Повертає курс
+     * @return курс
+     */
     public int getCourse() {
         return course;
     }
-// 4 курса бакалавра + 2 курса магістра =макс 6
+
+    /**
+     * Змінює курс
+     * @param course курс
+     */
     public void setCourse(int course) {
         this.course = course;
-        if (course>6) this.course=6;
+        if (course>6) this.course=6;  // 4 курса бакалавра + 2 курса магістра =макс 6
         if (course<1) this.course=1;
     }
 
+    /**
+     * Повертає групу
+     * @return група
+     */
     public int getGroup() {
         return group;
     }
+
+    /**
+     * Змінює групу
+     * @param group група
+     */
 //всього можно 2 групи
     public void setGroup(int group) {
         this.group = group;
@@ -32,6 +72,10 @@ public class Student extends Person{
         if (group<1) this.group=1;
     }
 
+    /**
+     * Виводить всю інформацію про студента
+     * @return
+     */
     public String toString(){
         return "Студент: "+getName()+" "+getLastName()
                 +", Факультет: "+ getPersonFaculty()+", Кафедра: "+getPersonDepartment()
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,8 +1,19 @@
 public class Teacher extends Person{
+    /**
+     * Конструктор
+     * @param name Ім^я
+     * @param lastName Прізвище
+     * @param personFaculty Факультет
+     * @param personDepartment Кафедра
+     */
     public Teacher(String name,String lastName,Faculty personFaculty,Department personDepartment){
         super(name,lastName,personFaculty,personDepartment);
     }
 
+    /**
+     * Виводить всю інформацію про викладача
+     * @return
+     */
     public String toString(){
         return "Викладач: "+getName()+" "+getLastName()
                 +", Факультет: "+ getPersonFaculty()+", Кафедра: "+getPersonDepartment();
