2299be68f5215650cb446a015882ba29ccef4e13
KKOULY <dmt.solovyov@gmail.com>
2020-06-02 18:07:51 +0300

    java doc Faculty

--- src/Faculty.java
+++ src/Faculty.java
@@ -1,29 +1,61 @@
 public class Faculty {
+    /**
+     *Назва факультету
+     */
     private String nameOfFaculty;
+
+    /**
+     * Масив кафедр
+     */
     private Department[] departments = new Department[0];
 
+    /**
+     * Конструктор факультету
+     * @param nameOfFaculty назва факультету
+     */
     public Faculty(String nameOfFaculty){
         this.nameOfFaculty = nameOfFaculty;
     }
 
+    /**
+     * Повертає назву факультету
+     * @return назва факультеу
+     */
     public String getNameOfFaculty() {
         return nameOfFaculty;
     }
 
+    /**
+     * Повертає масив кафедр
+     * @return
+     */
     public Department[] getDepartments() {
         return departments;
     }
 
+    /**
+     * Змінює масив кфаедр
+     * @param departments масив кафедр
+     */
     public void setDepartments(Department[] departments) {
         this.departments = departments;
     }
 
+    /**
+     * Створює і додає нову кфедру до масиву кафедр
+     * @param nameOfDepartment назва кафедри
+     */
     public void addDepartment(String nameOfDepartment){
         Department[] departmentsTemporary = new Department[departments.length+1];
         System.arraycopy(departments,0,departmentsTemporary,0,departments.length);
         departmentsTemporary[departments.length] = new Department(nameOfDepartment);
         departments = departmentsTemporary;
     }
+
+    /**
+     * Додає кафедру до масиву кафедр
+     * @param newDepartment кафедра
+     */
     public void addDepartment(Department newDepartment){
         Department[] departmentsTemporary = new Department[departments.length+1];
         System.arraycopy(departments,0,departmentsTemporary,0,departments.length);
@@ -31,6 +63,10 @@ public class Faculty {
         departments = departmentsTemporary;
     }
 
+    /**
+     * Видаляє кафедру за індексом
+     * @param numOfDepartment
+     */
     public void deleteDepartment(int numOfDepartment){
         if (numOfDepartment<=departments.length && numOfDepartment>=0) {
             Department[] departmentsTemporary = new Department[departments.length - 1];
@@ -45,19 +81,37 @@ public class Faculty {
         }
     }
 
+    /**
+     * Змінює назву кафедри
+     * @param nameOfFaculty
+     */
     public void setNameOfFaculty(String nameOfFaculty) {
         this.nameOfFaculty = nameOfFaculty;
     }
 
+    /**
+     * Повертає кафедру за індексом
+     * @param n індекс
+     * @return кафедра
+     */
     public Department departmentIndex(int n){
         if(n >=0 && n < departments.length){
             return departments[n];
         } else return null;
     }
+
+    /**
+     * Повертає довжину масиву кафедр
+     * @return
+     */
     public int departmentsLength(){
         return departments.length;
     }
 
+    /**
+     * Повертає назву факультету
+     * @return назва факультету
+     */
     public String toString(){
         return nameOfFaculty;
     }
